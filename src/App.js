75d3e25f674f0779261b8bf700194ed4595c5edf
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-09-11 13:10:56 -0300

    v0.0.20-beta.1300

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import { HashRouter, Redirect, Route } from 'react-router-dom';
-import { ipcRenderer } from 'electron';
-import { Layout } from 'antd';
-import { CornerDialog } from 'evergreen-ui';
+import styled from 'styled-components';
+import { ipcRenderer, remote } from 'electron';
+import { Layout, notification } from 'antd';
 import { AppMenu } from './components/App/Menu';
 import { Dashboard } from './views/Dashboard';
 import { Hotel } from './views/Hotel';
 import './App.css';
 
 export const App = () => {
-  // const electron = window.require('electron')
-  const [message, setMessage] = React.useState(' ');
-  const [showMessage, setShowMessage] = React.useState(false);
+  const Title = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: center;
+    height: 62px;
+    width: fit-content;
+    line-height: initial;
+  `;
+  const AppName = styled.span`
+    font-family: 'Comfortaa';
+    font-weight: 600;
+    font-size: 16px;
+  `;
+  const AppVersion = styled.span`
+    font-family: 'Comfortaa';
+    font-weight: 200;
+    font-size: 8px;
+  `;
+  const [appVersion] = React.useState(remote.app.getVersion());
   ipcRenderer.on('message', (event, text) => {
-    setShowMessage(true);
-    setMessage(text);
+    notification.info({
+      message: 'Paradise Client',
+      description: text,
+      placement: 'bottomRight'
+    });
   });
-
-  // const STMenu = styled(Menu)`
-  // height: 100%;
-  // max-width: 200px;
-  // `
-  // const Layout = styled(layout)`
-  //   position: absolute;
-  //   height: 100%;
-  //   background-color: #f5f5f5;
-  // `
-  // const Content = styled(layout.Content)`
-  // width: 100%;
-  // `
 
   return (
     <HashRouter>
@@ -39,17 +46,18 @@ export const App = () => {
       >
         <AppMenu />
         <Layout>
+          <Layout.Header style={{ background: '#f0f2f5' }}>
+            <Title>
+              <AppName>Paradise Client</AppName>
+              <AppVersion>Versión: {appVersion}</AppVersion>
+            </Title>
+          </Layout.Header>
           <Layout.Content>
-            {/* <Switch> */}
             <Redirect from="/" to="dashboard/" />
             <Route path="/dashboard" component={Dashboard} />
             <Route path="/hotel" component={Hotel} />
-            {/* </Switch> */}
           </Layout.Content>
         </Layout>
-        <CornerDialog title="Paradise Client" isShown={showMessage}>
-          {message}
-        </CornerDialog>
       </Layout>
     </HashRouter>
   );
